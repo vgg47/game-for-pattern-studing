fdc990fb30b1c66783a12e08b550e8fc4ebaf099
elevely <vgg47@mail.ru>
2018-04-09 17:44:24 +0300

    Добавил гондорского лучника

--- python/Gondor_pers.py
+++ python/Gondor_pers.py
@@ -1,10 +1,18 @@
 from Unit_hierarchy import Gondor, Damage_dealer, Support 
 
-class Soldier(Gondor, Damage_dealer):
+class Gondor_soldier(Gondor, Damage_dealer):
 	"""docstring for Soldier"""
 	def __init__(self, **characteristics):
-		super(Soldier, self).__init__(**characteristics)
+		super().__init__(**characteristics)
 	
 	@staticmethod
 	def message():
-		print("New soldier is created")
+		print("New Gondor Soldier is created")
+
+class Gondor_archer(Gondor, Damage_dealer):
+	def __init__(self, **characteristics):
+		super().__init__(**characteristics)
+
+	@staticmethod
+	def message():
+		print("New Gondor Archer is created")
--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
 from Gondor_pers import Soldier
 
-a = Soldier( magic_spell = 1, attack = 1, attack_range = 1, attack_speed = 1,\
+a = Gondor_soldier( magic_spell = 1, attack = 1, attack_range = 1, attack_speed = 1,\
  attack_type = 1, cost = 1, health = 1, speed = 1, defence = 1, magic_resistance = 1, phys_resistance = 1)
