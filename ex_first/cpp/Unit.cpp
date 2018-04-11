4a857376a8b467723cd86aab37d8288ebb7e27f7
elevely <vgg47@mail.ru>
2018-04-12 02:19:02 +0300

    Delete c++ version

--- ex_first/cpp/Unit.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include "../h/Unit.h"
-
-void Unit::Unit(int cost, int _health, int _speed, int _defence, int _magic_resistance, int _phys_resistance):
-cost(_cost), health(_health), speed(_speed), defence(_defence), magic_resictance(_magic_resistance), phys_resistance(_phys_resistance) {}
--- ex_first/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "/h/Factory_pers.h"
-
-using namespace std;
-
-int main() {
-	unique_ptr<Knight> a = create_knight();
-}
